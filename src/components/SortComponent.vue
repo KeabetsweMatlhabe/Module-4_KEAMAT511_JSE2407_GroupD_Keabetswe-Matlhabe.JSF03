917a67932fc0eb07b4a851de556171551abc770f
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-08-05 09:34:32 +0200

    renamed style.css to main.css

---
+++ src/components/SortComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex items-center space-x-4">
+      <select
+        v-model="selectedSort"
+        @change="sortProducts"
+        class="p-2 border rounded"
+      >
+        <option value="">Sort by</option>
+        <option value="priceAsc">Price: Low to High</option>
+        <option value="priceDesc">Price: High to Low</option>
+      </select>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      onSort: Function,
+    },
+    data() {
+      return {
+        selectedSort: "",
+      };
+    },
+    methods: {
+      sortProducts() {
+        this.onSort(this.selectedSort);
+      },
+    },
+  };
+  </script>
