63a9101bf4758632417af9bbbfb6097c9d8a3f1f
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-08-06 09:36:48 +0200

    created a path for productDetail in the index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/HomePage.vue';
 import WishList from '../views/WishList.vue';
 import Login from '../views/LoginPage.vue';
 import Cart from '../views/CartPage.vue';
+import ProductDetailPage from '../views/ProductDetailPage.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/wishList', name: 'WishList', component: WishList },
   { path: '/login', name: 'Login', component: Login },
   { path: '/cart', name: 'Cart', component: Cart },
+  { path: '/products/:id', name: 'ProductDetail', component: ProductDetailPage },
 ];
 
 const router = createRouter({
